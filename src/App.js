6a410651630d6a6d110080f7c2011627dae2bc0d
赵煜 <1490365616@qq.com>
2021-02-24 20:05:45 +0800

    测试功能

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+/*
+ * @Description:
+ * @Version: 2.0
+ * @Autor: zhaoyu
+ * @Date: 2021-02-24 19:53:36
+ * @LastEditors: zhaoyu
+ * @LastEditTime: 2021-02-24 20:05:13
+ */
+import Main from "./views/main/index";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Main />;
 }
 
 export default App;
---
+++ src/views/main/index.js
@@ -0,0 +1,16 @@
+/*
+ * @Description:
+ * @Version: 2.0
+ * @Autor: zhaoyu
+ * @Date: 2021-02-24 20:03:17
+ * @LastEditors: zhaoyu
+ * @LastEditTime: 2021-02-24 20:04:32
+ */
+
+import React from "react";
+
+export default class Main extends React.Component {
+  render() {
+    return <h1>hello world</h1>;
+  }
+}
